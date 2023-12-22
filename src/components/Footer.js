1046e83a15584323392ab2e6027fb3586c495564
Mark Conroy <mark@mark.ie>
2023-12-22 12:15:52 +0000

    sets footer to use grid component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import { Logo } from './Logo.js';
 import { Container } from './Container.js';
+import { Grid, GridItem } from './Grid.js';
 
 export class Footer extends HTMLElement {
   static {
@@ -17,7 +18,7 @@ export class Footer extends HTMLElement {
           padding-block: var(--mc-spacing-largest);
         }
         .mc-footer__nav {
-          margin-block-start: var(--mc-spacing);
+          margin-block-start: var(--mc-spacing-largest);
         }
         .mc-footer__menu ul {
           list-style: none;
@@ -49,48 +50,63 @@ export class Footer extends HTMLElement {
         }
       </style>
       <footer class="mc-footer">
-        <mc-container padding="inline" size="100">
+        <mc-container padding="inline">
           <div class="mc-footer__inner">
+
             <div class="mc-footer__logo">
               <mc-logo></mc-logo>
             </div>
+
             <nav class="mc-footer__nav">
-              <div class="mc-grid mc-grid--quarters">
-                <div class="mc-footer__menu">
-                  <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Our Work</a></li>
-                    <li><a href="#">Our Services</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                  </ul>
-                </div>
-                <div class="mc-footer__menu">
-                  <ul>
-                    <li><a href="#">Products</a></li>
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Returns</a></li>
-                  </ul>
-                </div>
-                <div class="mc-footer__menu">
-                  <ul>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Events</a></li>
-                  </ul>
-                </div>
-                <div class="mc-footer__menu">
-                  <ul>
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Instagram</a></li>
-                    <li><a href="#">TikTok</a></li>
-                    <li><a href="#">YouTube</a></li>
-                  </ul>
-                </div>
-              </div>
+              <mc-grid>
+
+                <mc-grid-item size="one-quarter">
+                  <div class="mc-footer__menu">
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Our Work</a></li>
+                      <li><a href="#">Our Services</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                    </ul>
+                  </div>
+                </mc-grid-item>
+
+                <mc-grid-item size="one-quarter">
+                  <div class="mc-footer__menu">
+                    <ul>
+                      <li><a href="#">Products</a></li>
+                      <li><a href="#">Pricing</a></li>
+                      <li><a href="#">FAQs</a></li>
+                      <li><a href="#">Returns</a></li>
+                    </ul>
+                  </div>
+                </mc-grid-item>
+
+                <mc-grid-item size="one-quarter">
+                  <div class="mc-footer__menu">
+                    <ul>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">News</a></li>
+                      <li><a href="#">Events</a></li>
+                    </ul>
+                  </div>
+                </mc-grid-item>
+
+                <mc-grid-item size="one-quarter">
+                  <div class="mc-footer__menu">
+                    <ul>
+                      <li><a href="#">GitHub</a></li>
+                      <li><a href="#">Twitter</a></li>
+                      <li><a href="#">LinkedIn</a></li>
+                      <li><a href="#">Instagram</a></li>
+                      <li><a href="#">TikTok</a></li>
+                      <li><a href="#">YouTube</a></li>
+                    </ul>
+                  </div>
+                </mc-grid-item>
+
+              </mc-grid>
             </nav>
             <div class="mc-footer__credits">
               <p>Created by <a href="https://mark.ie">Mark Conroy</a> | Code available on <a href="https://github.com/markconroy/web-components-design-system">GitHub</a></p>
