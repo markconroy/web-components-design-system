8d7c44735d001debb286888cb1561b674b42ab4a
Mark Conroy <mark@mark.ie>
2023-12-22 12:08:17 +0000

    converts grids to web component

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 import { Header } from './src/components/Header.js';
 import { Footer } from './src/components/Footer.js';
 import { Container } from './src/components/Container.js';
+import { Grid, GridItem } from './src/components/Grid.js';
---
+++ src/components/Grid.js
@@ -0,0 +1,80 @@
+export class Grid extends HTMLElement {
+  static {
+    customElements.define("mc-grid", this);
+  }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+        @import url('../style.css');
+        :host {
+          display: grid;
+          gap: var(--mc-grid-gap);
+          grid-template-columns: repeat(var(--mc-grid-columns), 1fr);
+        }
+        :host([gap="none"]) {
+          gap: var(--mc-grid-gap-none);
+        }
+        :host([gap="small"]) {
+          gap: var(--mc-grid-gap-small);
+        }
+        :host([gap="medium"]) {
+          gap: var(--mc-grid-gap);
+        }
+        :host([gap="large"]) {
+          gap: var(--mc-grid-gap-large);
+        }
+      </style>
+      <slot></slot>
+    `;
+  }
+}
+
+export class GridItem extends HTMLElement {
+  static {
+    customElements.define("mc-grid-item", this);
+  }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host() {
+          grid-column: span var(--mc-grid-columns);
+        }
+        :host([size="one-quarter"]),
+        :host([size="one-third"]),
+        :host([size="one-half"]),
+        :host([size="two-thirds"]),
+        :host([size="three-quarters"]) {
+          grid-column: span var(--mc-grid-columns);
+        }
+        @media screen and (min-width: 48em) {
+          :host([size="one-quarter"]),
+          :host([size="one-third"]),
+          :host([size="one-half"]),
+          :host([size="two-thirds"]),
+          :host([size="three-quarters"]) {
+            grid-column: span calc(var(--mc-grid-columns) / 2);
+          }
+        }
+        @media screen and (min-width: 60em) {
+          :host([size="one-third"]) {
+            grid-column: span calc(var(--mc-grid-columns) / 3);
+          }
+          :host([size="two-thirds"]) {
+            grid-column: span calc(var(--mc-grid-columns) / 3 * 2);
+          }
+          :host([size="one-quarter"]) {
+            grid-column: span calc(var(--mc-grid-columns) / 4);
+          }
+          :host([size="three-quarters"]) {
+            grid-column: span calc(var(--mc-grid-columns) / 4 * 3);
+          }
+        }
+      </style>
+      <slot></slot>
+    `;
+  }
+}
